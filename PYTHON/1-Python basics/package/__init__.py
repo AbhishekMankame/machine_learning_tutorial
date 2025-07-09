a343cbb7c9d7b675b6180948723e50eb445ca036
abhishek <abhishekmankame962@gmail.com>
2025-07-09 11:14:04 +0530

    import modules update

---
+++ PYTHON/1-Python basics/package/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py is a special file used in Python to define packages and intialize their namespaces
+
+# Note: If we need to initialize the entire folder and define packages or modules then we have to use __init__.py, even though it is empty keep it empty
