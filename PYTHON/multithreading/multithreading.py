5096304d9c000965f115bd707cc431e4e13a4805
abhishek <abhishekmankame962@gmail.com>
2025-07-10 21:16:09 +0530

    Multithreading example

--- PYTHON/multithreading/multithreading.py
+++ PYTHON/multithreading/multithreading.py
@@ -8,14 +8,25 @@ import time
 
 def print_numbers():
     for i in range(5):
+        time.sleep(2)
         print(f"Number:{i}")
 
 def print_letter():
     for letter in "abcde":
+        time.sleep(2)
         print(f"Letter: {letter}")
 
+## create two threads
+t1 = threading.Thread(target=print_numbers)
+t2 = threading.Thread(target=print_letter)
+
 t=time.time()
-print_numbers()
-print_letter()
+## Start the thread
+t1.start()
+t2.start()
+
+## Wait for the threads to completed
+t1.join()
+t2.join()
 finished_time=time.time()-t
 print(finished_time)
