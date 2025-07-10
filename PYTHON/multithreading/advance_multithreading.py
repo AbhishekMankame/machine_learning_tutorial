12f6ab87b51f77d1f719c7be6405532012bbe849
abhishek <abhishekmankame962@gmail.com>
2025-07-10 22:33:57 +0530

    failed code update

--- PYTHON/multithreading/advance_multithreading.py
+++ PYTHON/multithreading/advance_multithreading.py
@@ -7,9 +7,9 @@ def print_number(number):
     time.sleep(1)
     return f"Number :{number}"
 
-numbers = [1,2,3,4,5]
+numbers = [1,2,3,4,5,6,7,8,9,10,1,11,21,123]
 
-with ThreadPoolExecutor(max_workers=3) as executor:
+with ThreadPoolExecutor(max_workers=2) as executor:
     results=executor.map(print_number,numbers)
 
 for result in results:
