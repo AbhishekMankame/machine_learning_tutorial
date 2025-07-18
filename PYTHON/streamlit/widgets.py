40308b1778ac4ed3631ab135ecd718ce0095f1f6
abhishek <abhishekmankame962@gmail.com>
2025-07-18 23:48:46 +0530

    select box update

--- PYTHON/streamlit/widgets.py
+++ PYTHON/streamlit/widgets.py
@@ -9,5 +9,9 @@ name = st.text_input("Enter your name:")
 age = st.slider("Select your age:",0,100,25)
 st.write(f"Your age is {age}.")
 
+options = ["Python", "Java", "C++", "JavaScript"]
+choice = st.selectbox("Choose your favorite language:", options)
+st.write(f"You selected {choice}")
+
 if name:
     st.write(f"Hello, {name}")
