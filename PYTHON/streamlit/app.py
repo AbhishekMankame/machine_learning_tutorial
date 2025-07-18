07887dae2eeb95aaaa4b5c74624c866a531b9a7e
abhishek <abhishekmankame962@gmail.com>
2025-07-18 23:34:20 +0530

    streamlit code update

---
+++ PYTHON/streamlit/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+## Title pf the application
+st.title("Hello Streamlit")
+
+## Display a simple text
+st.write("This is a simple text")
+
+## Create a simple dataframe
+
+df = pd.DataFrame({
+    'first column': [1,2,3,4],
+    'second column':[10,20,30,40]
+})
+
+## Display the Dataframe
+st.write("Here is the dataframe")
+st.write(df)
